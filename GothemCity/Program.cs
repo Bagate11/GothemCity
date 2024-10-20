d473c9533f2ae2bc0eb9e401a8b6dc185ff11c63
Administratör <Administratr@5CD022GKGS>
2024-10-20 13:32:16 +0200

    Add project files.

---
+++ GothemCity/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GothemCity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+    public class Sak
+    { 
+        public string Name { get; set; }
+}
