daf9653fc64949fbe4256ec6add56bfc8bcbb08f
Bagate11 <ermias03555@gmail.com>
2024-10-25 14:57:21 +0200

    Update Program.cs

--- GothemCity/Program.cs
+++ GothemCity/Program.cs
@@ -1,6 +1,5 @@
-﻿namespace GothemCity
+namespace GothemCity
 {
- 
     public class Sak
     {
         public string Name { get; set; }
@@ -8,98 +7,85 @@
         {
             Name = name;
         }
-
     }
+
     public abstract class Person
     {
         public int X { get; set; }
         public int Y { get; set; }
-        public int XVägg { get; set; }
-        public int YVägg { get; set; }
+        public int DirectionX { get; set; }
+        public int DirectionY { get; set; }
 
-        public List<Sak> Inventory { get; set; }
+        public List<Sak> Lagra { get; set; }
         protected static Random random = new Random();
-
+        public bool Fängslad { get; set; }
         public Person(int x, int y)
         {
             X = x;
             Y = y;
-            Inventory = new List<Sak>();
+            Lagra = new List<Sak>();
+            Fängslad = false;
             RandomDirection();
-
         }
 
         public void RandomDirection()
         {
             var directions = new (int, int)[]
             {
-                (-1, 0),
-                (1, 0),
-                (0, -1),
-                (0, 1),
-                (-1, 1),
-                (1, -1),
-
+                (-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, -1),
             };
 
             var dir = directions[random.Next(directions.Length)];
-            XVägg = dir.Item1;
-            YVägg = dir.Item2;
+            DirectionX = dir.Item1;
+            DirectionY = dir.Item2;
         }
 
-        public void Move()
+        public virtual void Move()
         {
-            X += XVägg;
-            Y += YVägg;
+            if (!Fängslad)
+            {
+                X += DirectionX;
+                Y += DirectionY;
 
-            if (X < 0) X = 99;
-            if (X > 99) X = 0;
-            if (Y < 0) Y = 24;
-            if (Y > 24) Y = 0;
+                if (X < 0) X = 99;
+                if (X > 99) X = 0;
+                if (Y < 0) Y = 24;
+                if (Y > 24) Y = 0;
+            }
         }
-   
-        
 
         public abstract void Kontakt(Person other);
-
         public abstract char Symbol();
-
     }
+
     public class Medborgare : Person
     {
         public Medborgare(int x, int y) : base(x, y)
         {
-            Inventory.Add(new Sak("Nycklar"));
-            Inventory.Add(new Sak("Telefon"));
-            Inventory.Add(new Sak("Pengar"));
-            Inventory.Add(new Sak("ROLEX"));
+            Lagra.Add(new Sak("Nycklar"));
+            Lagra.Add(new Sak("Telefon"));
+            Lagra.Add(new Sak("Pengar"));
+            Lagra.Add(new Sak("ROLEX"));
         }
 
         public override void Kontakt(Person other)
         {
             if (other is Tjuv tjuv)
             {
-                if (Inventory.Count > 0)
+                if (Lagra.Count > 0)
                 {
-                    var stulen = Inventory[random.Next(Inventory.Count)];
-                    Inventory.Remove(stulen);
-                    tjuv.Inventory.Add(stulen);
+                    var stulen = Lagra[random.Next(Lagra.Count)];
+                    Lagra.Remove(stulen);
+                    tjuv.Lagra.Add(stulen);
                     Console.WriteLine($"Tjuven rånade Medborgaren och tog {stulen.Name}.");
-                    Thread.Sleep(2000);
-
+                    Thread.Sleep(1000);
                 }
             }
-
-
             else if (other is Polis)
             {
-                Console.WriteLine("Medboragren Mötte en Polis, Dom hälsade på varandra");
-                Thread.Sleep(2000);
-
-
+                Console.WriteLine("Medboragren mötte en Polis, de hälsade på varandra.");
+                Thread.Sleep(1000);
             }
-
-
         }
 
         public override char Symbol()
@@ -107,10 +93,6 @@
             return 'M';
         }
     }
-
-
-
-
 
     public class Tjuv : Person
     {
@@ -120,137 +102,155 @@
         {
             if (other is Polis polis)
             {
-                polis.Inventory.AddRange(Inventory);
-                Inventory.Clear();
-                Console.WriteLine("Polisen har grippit Tjuven och tog allt vad Tjuven hade stulit!");
-                Thread.Sleep(2000);
-
-
-
+                polis.Lagra.AddRange(Lagra);
+                Lagra.Clear();
+                Console.WriteLine("Polisen har gripit Tjuven och tagit allt stöldgods!");
+                Thread.Sleep(500);
+                Fängelse();
             }
+        }
 
+        public void Fängelse()
+        {
+            
+            X = Stad.FängelseXStart + Stad.random.Next(Stad.FängelseXSlut - Stad.FängelseXStart);
+            Y = Stad.FängelseYStart + Stad.random.Next(Stad.FängelseYSlut - Stad.FängelseYStart);
+            Fängslad = true;
+            Console.WriteLine($"Tjuven har blivit satt i fängelset på ({X}, {Y}).");
+        }
+
+        public override void Move()
+        {
+            if (Fängslad)
+            {
+                
+                X += DirectionX;
+                Y += DirectionY;
+
+                if (X < Stad.FängelseXStart) X = Stad.FängelseXSlut - 1;
+                if (X >= Stad.FängelseXSlut) X = Stad.FängelseXStart;
+                if (Y < Stad.FängelseYStart) Y = Stad.FängelseYSlut - 1;
+                if (Y >= Stad.FängelseYSlut) Y = Stad.FängelseYStart;
+
+                Console.WriteLine($"Tjuven är i fängelset och rör sig till ({X}, {Y}).");
+            }
+            else
+            {
+                base.Move(); 
+            }
         }
 
         public override char Symbol()
         {
-            return 'T';
+            return Fängslad ? 'F' : 'T';
         }
-
-
     }
 
-        public class Polis : Person
+    public class Polis : Person
+    {
+        public Polis(int x, int y) : base(x, y) { }
+
+        public override void Kontakt(Person other)
         {
-            public Polis(int x, int y) : base(x, y) { }
-
-            public override void Kontakt(Person other)
+            if (other is Tjuv tjuv && !tjuv.Fängslad)
             {
-                if (other is Tjuv tjuv)
-                {
-                    Inventory.AddRange(tjuv.Inventory);
-                    tjuv.Inventory.Clear();
-                    Console.WriteLine("Polisen tog tjuvrn och beslagtog Tjuvens stulna grejer");
-                    Thread.Sleep(2000);
-
-                }
-            }
-
-            public override char Symbol()
-            {
-                return 'P';
+                Lagra.AddRange(tjuv.Lagra);
+                tjuv.Lagra.Clear();
+                Console.WriteLine("Polisen tog Tjuven och beslagtog alla stulna föremål.");
+                Thread.Sleep(2000);
+                tjuv.Fängelse();
             }
         }
 
-        class Stad
+        public override char Symbol()
         {
-            static void Main(string[] args)
+            return 'P';
+        }
+    }
+
+    class Stad
+    {
+        public static int FängelseXStart = 100;
+        public static int FängelseXSlut= 110;
+        public static int FängelseYStart = 0;
+        public static int FängelseYSlut = 5;
+
+        public static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            List<Person> personer = new List<Person>();
+            Random random = new Random();
+
+            int StadBreed = 100;
+            int StadHöjd = 25;
+
+            
+            for (int i = 0; i < 30; i++)
+                personer.Add(new Medborgare(random.Next(StadBreed), random.Next(StadHöjd)));
+            for (int h = 0; h < 20; h++)
+                personer.Add(new Tjuv(random.Next(StadBreed), random.Next(StadHöjd)));
+            for (int i = 0; i < 10; i++)
+                personer.Add(new Polis(random.Next(StadBreed), random.Next(StadHöjd)));
+
+            while (true)
             {
-                List<Person> personer = new List<Person>();
-                Random random = new Random();
-                int AntalRånadeMedborgare = 0;
-                int AntalGripnaTjvar = 0;
-                int StadBreed = 100;
-                int StadHöjd = 25;
-
-                for (int f = 0; f < 50; f ++) // ändra det från i till f 
-                    personer.Add(new Medborgare(random.Next(StadBreed), random.Next(StadHöjd)));
-
-                for (int a = 0; a < 20; a ++) // ändra det från h till a 
-                    personer.Add(new Tjuv(random.Next(StadBreed), random.Next(StadHöjd)));
-                for (int j = 0; j < 15;  j ++)
-                    personer.Add(new Polis(random.Next(StadBreed), random.Next(StadHöjd)));
-
-                while (true)
+                foreach (var person in personer)
                 {
-                    foreach (var person in personer)
-                    {
-                        person.Move();
-                    }
+                    person.Move();
+                }
 
-                    for (int i = 0; i < personer.Count; i++)
+                for (int i = 0; i < personer.Count; i++)
+                {
+                    for (int j = i + 1; j < personer.Count; j++)
                     {
-
-                        for (int j = i + 1; j < personer.Count; j++)
+                        if (personer[i].X == personer[j].X && personer[i].Y == personer[j].Y)
                         {
-
-                            if (personer[i].X == personer[j].X && personer[i].Y == personer[j].Y)
-                            {
-                                personer[i].Kontakt(personer[j]);
-                                personer[j].Kontakt(personer[i]);
-
-                                if (personer[i] is Tjuv && personer[j] is Medborgare)
-                                {
-                                    AntalRånadeMedborgare++;
-                                }
-
-                                else if (personer[i] is Polis && personer[j] is Tjuv)
-                                {
-                                    AntalGripnaTjvar++;
-                                }
-
-                                Thread.Sleep(2000);
-                            }
+                            personer[i].Kontakt(personer[j]);
+                            personer[j].Kontakt(personer[i]);
+                            Thread.Sleep(2000);
                         }
                     }
+                }
 
-                    Console.Clear();
-                    Console.WriteLine($"Antal rånade medborgare {AntalRånadeMedborgare}   ");
-                    Console.WriteLine($"antal gripna tjuvar {AntalGripnaTjvar}");
+                Console.Clear();
+                char[,] stad = new char[StadBreed + 10, StadHöjd];
 
-                    char[,] stad = new char[StadBreed, StadHöjd];
-                    for (int y = 0; y < StadHöjd; y++)
-                    {
-                        for (int x = 0; x < StadBreed; x ++)
-                        {
-                        Console.Write (stad[x, y] = '.' );
-                        }
-                    Console.WriteLine();
-                    }
-                    foreach (var person in personer)
-                    {
-                        stad[person.X, person.Y] = person.Symbol();
-                    }
-                   
-                
-                
                 for (int y = 0; y < StadHöjd; y++)
+                {
+                    for (int x = 0; x < StadBreed; x++)
                     {
-                        for (int x = 0; x < StadBreed; x++)
-                        {
-                            Console.Write(stad[x, y ]);
-                        }
-                        Console.WriteLine();
+                        stad[x, y] = '.';
                     }
-
-                    Thread.Sleep(500);
                 }
+
+                for (int y = Stad.FängelseYStart; y < Stad.FängelseYSlut; y++)
+                {
+                    for (int x = Stad.FängelseXStart; x < Stad.FängelseXSlut; x++)
+                    {
+                        stad[x, y] = '#';
+                    }
+                }
+
+                foreach (var person in personer)
+                {
+                    stad[person.X, person.Y] = person.Symbol();
+                }
+
+                for (int y = 0; y < StadHöjd; y++)
+                {
+                    for (int x = 0; x < StadBreed + 10; x++)
+                    {
+                        Console.Write(stad[x, y]);
+                    }
+                    Console.WriteLine();
+                }
+
+                Thread.Sleep(500);
             }
         }
     }
-
-
-                    
-
+}
 
                 
             
